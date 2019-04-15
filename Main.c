42f608c61fd96a050f2c91b658a647c4ca3a25fb
liaoyongshuang <liaoyongshuang@sangrei.com>
2019-04-15 18:20:35 +0800

    add doc file - ht2900 project

--- Main.c
+++ Main.c
@@ -73,6 +73,148 @@ void showProgressBar(uint8 y, uint32 maxValue, uint32 currValue)
 	_GUIRectangleFill(0, y, width, y + 16, 1);
 }
 
+/*
+* 函数名：StringPadLeft
+* 描  述：字符串左侧填充
+* 参  数：srcStr - 原字符串
+		  totalLen - 总字符长度：原字符+左侧填充的字符（若原字符长度>=总长度，则无需填充）
+		  padChar - 填充的字符
+* 返回值：char * 左侧填充后的字符串
+*/
+char * StringPadLeft(const char * srcStr, int totalLen, char padChar)
+{
+	uint32 srcStrLen, i = 0;
+	char * newStr = NULL;
+
+	srcStrLen = strlen(srcStr);
+	if(srcStrLen >= totalLen){
+		return srcStr;
+	}
+	if(padChar == 0x00){
+		return NULL;
+	}
+	
+	newStr = (char *)malloc(totalLen + 1);
+	for(i = 0; i < (totalLen - srcStrLen); i++){
+		newStr[i] = padChar;
+	}
+	while(i < totalLen){
+		newStr[i] = srcStr[i];
+		i++;
+	}
+	newStr[i] = 0x00;
+
+	return newStr;
+}
+
+/*
+* 函数名：HexToChar
+* 描  述：16进制数转换成对应的字符
+*/
+char HexToChar(uint8 b)
+{
+	char decHex[16] = {'0', '1', '2', '3','4', '5', '6', '7','8', '9', 'A', 'B','C', 'D', 'E', 'F'};
+	
+	if(b < 16){
+		return decHex[b];
+	}else{
+		return '\0';
+	}
+}
+/*
+* 函数名：CharToHex
+* 描  述：字符转换成对应的16进制数
+*/
+uint8 CharToHex(char c)
+{
+	uint8 hex;
+	
+	if(c >= '0' && c <= '9'){
+		hex = c - '0';
+	}else if(c >= 'A' && c <= 'F'){
+		hex = c - 'A' + 10;
+	}else if(c >= 'a' && c <= 'f'){
+		hex = c - 'a' + 10;
+	}else{
+		hex = 0xFF;
+	}
+
+	return hex;
+}
+
+char * GetStringHexFromBytes(uint8 bytes[], int iStart, int iLength, char strSeparate, uint8 Reverse)
+{
+	char * strHex = "";
+	uint8 aByte;
+	int iLoop;
+   
+	if(bytes == NULL || iStart + iLength > sizeof(bytes) || iStart < 0){
+		return strHex;
+	}
+
+	if(strSeparate != 0){
+		strHex = (char *)malloc(iLength * 3);
+	}else{
+		strHex = (char *)malloc(iLength * 2);
+	}
+	
+	for (iLoop = 0; iLoop < iLength; iLoop++)
+	{
+		if (Reverse){
+			aByte = bytes[iStart + iLength - 1 - iLoop];
+		}
+		else{
+			aByte = bytes[iStart + iLoop];
+		}
+		strHex[iLoop] = HexToChar(aByte >> 4);
+		strHex[iLoop + 1] = HexToChar(aByte & 0x0F);
+		if(strSeparate != 0){
+			strHex[iLoop + 2] = strSeparate;
+		}
+	}
+
+	return strHex;
+}
+
+int GetBytesFromStringHex(uint8 bytes[], int iStart, const char * strHex, char strSeparate, uint8 Reverse)
+{
+	int index = 0, bytesLen, strHexLen;
+	uint8 aByte;
+	int iLoop;
+  
+	strHexLen = strlen(strHex);
+	if(strSeparate != 0){
+		bytesLen = (strHexLen + 1) / 3;
+	}else{
+		bytesLen = (strHexLen + 1) / 2;
+	}
+
+	if(bytes == NULL || iStart + bytesLen > sizeof(bytes) || iStart < 0){
+		return 0;
+	}
+
+	for (iLoop = 0; iLoop < strHexLen; iLoop++)
+	{
+		aByte = (CharToHex(strHex[iLoop]) << 4) + (CharToHex(strHex[iLoop + 1]) & 0x0F);
+		iLoop += 2;
+
+		if (Reverse){
+			bytes[iStart + bytesLen - 1 - index] = aByte;
+		}
+		else{
+			bytes[iStart + index] = aByte;
+		}
+	
+		if(strSeparate != 0){
+			iLoop++;
+		}
+		index++;
+	}
+
+	return index;
+}
+
+
 // --------------------------------  参数配置/读取  -----------------------------------------
 
 
@@ -95,7 +237,10 @@ void ElectricSubNodeFunc(void)
 {
 	uint8 key, menuItemNo, tryCnt = 0;
 	_GuiLisStruEx menuList;
+	_GuiInputBoxStru inputBox;
+	uint8 inputLen;
 	int index;
+	char * zeroAddr = "00000000000000";	// 0 地址，14字符
 	
 	_ClearScreen();
 
@@ -111,7 +256,16 @@ void ElectricSubNodeFunc(void)
 	menuList.str[1] = "  2. 读取节点配置";
 	menuList.str[2] = "  3. 645-07抄表";
 	menuList.defbar = 1;
-	_GUIHLine(0, 4*16 + 8, 160, 1);
+	//_GUIHLine(0, 4*16 + 8, 160, 1);
+
+	// 输入框
+	inputBox.top = 1 * 16;
+	inputBox.left = 5 * 16;
+	inputBox.width = 5 * 16;
+	inputBox.hight = 16;
+	inputBox.type = 1;	// 数字
+	inputBox.keyUpDown = 1;
+	inputBox.IsClear = 0;
 
 	_CloseCom();
 	_ComSetTran(3);
@@ -119,42 +273,67 @@ void ElectricSubNodeFunc(void)
 
 	while(1){
 
+		_ClearScreen();
 		menuItemNo = _ListEx(&menuList);
 
 		if (menuItemNo == 0){
 			break;
 		}
 		menuList.defbar = menuItemNo;
+		_ClearScreen();
 
 		switch(menuItemNo){
 		case 1:		// " 读取软件版本 ";
-			_GUIRectangleFill(0, 5 * 16, 160, 9 * 16, 0);
-			txLen = 0;
-			ComBuf[txLen++] = 0xAA;
-			ComBuf[txLen++] = 0xBB;
-			ComBuf[txLen++] = 0x01;
-			ComBuf[txLen++] = 0x07;
-			ComBuf[txLen++] = 0xCC;
-			_GetComStr(TmpBuf, 1024, 10);	// clear , 100ms timeout
-			_SendComStr(ComBuf, txLen);
-			_Printfxy(0, 5*16, "查询中...", 0);
+			_Printfxy(0, 0, "电表>>读取软件版本", 0);
+			_Printfxy(0, 1*16, "电表地址:", 0);
+			_Printfxy(0, 9*16, "返回            确定", 0);
 
-			sprintf(ComBuf, "当前版本:");
-			rxLen = _GetComStr(&ComBuf[9], 50, 50);	// recv , 500ms timeout
-			if(rxLen < 30 || strncmp(&ComBuf[9], "SRWF-", 5) != 0)
+			while(1)
 			{
-				_Printfxy(0, 5*16, "接收超时", 0);
-				break;
+				key = _InputBox(&inputBox);
+				if (key == KEY_CANCEL)
+					break;
+
+				inputLen = strlen(inputBox.context);
+				if(inputBox.datelen == 0 || strncmp(zeroAddr, inputBox.context, inputLen) == 0){
+					_Printfxy(0, 3*16, "请先输入有效地址", 0);
+				}else{
+					_Printfxy(0, 3*16, "                ", 0);
+				}
+				inputBox.context = StringPadLeft(inputBox.context, 12, '0');
+
+				GetBytesFromStringHex(TmpBuf, 0, inputBox.context, 0, 1);
+				_Printfxy(0, 3*16, &TmpBuf[0], 0);
+				
+				txLen = 0;
+				ComBuf[txLen++] = 0xAA;
+				ComBuf[txLen++] = 0xBB;
+				ComBuf[txLen++] = 0x01;
+				ComBuf[txLen++] = 0x07;
+				ComBuf[txLen++] = 0xCC;
+				_GetComStr(TmpBuf, 1024, 10);	// clear , 100ms timeout
+				_SendComStr(ComBuf, txLen);
+				_Printfxy(0, 2*16, "已发送...        ", 0);
+
+				sprintf(ComBuf, "版本信息:");
+				rxLen = _GetComStr(&ComBuf[9], 50, 50);	// recv , 500ms timeout
+				if(rxLen < 30 || strncmp(&ComBuf[9], "SRWF-", 5) != 0)
+				{
+					_Printfxy(0, 2*16, "接收超时       ", 0);
+					break;
+				}
+				_Printfxy(0, 5*16, &ComBuf[0], 0);
+				_Printfxy(0, 6*16, &ComBuf[20], 0);
+				_Printfxy(0, 7*16, &ComBuf[40], 0);
+
+				_ReadKey();
 			}
-			_Printfxy(0, 5*16, &ComBuf[0], 0);
-			_Printfxy(0, 6*16, &ComBuf[20], 0);
-			_Printfxy(0, 7*16, &ComBuf[40], 0);
 			break;
 
 		case 2:		// " 读取节点配置 "
 			_GUIRectangleFill(0, 5 * 16, 160, 9 * 16, 0);
 
-			sprintf(TmpBuf, "文件: %s\0", fileName);
+			//sprintf(TmpBuf, "文件: %s\0", fileName);
 			_Printfxy(0, 5*16, &TmpBuf[0], 0);
 			_Printfxy(0, 6*16, &TmpBuf[20], 0);
 
@@ -164,8 +343,8 @@ void ElectricSubNodeFunc(void)
 			_GUIRectangleFill(0, 5 * 16, 160, 9 * 16, 0);
 			
 
-			sprintf(tmp, "总包数: %d\0", totalCnt);
-			_Printfxy(0, 5*16, &tmp[0], 0);
+			//sprintf(tmp, "总包数: %d\0", totalCnt);
+			//_Printfxy(0, 5*16, &tmp[0], 0);
 
 			
 			break;
